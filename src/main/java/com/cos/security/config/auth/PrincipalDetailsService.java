859d934834abb556b89bc756b43b50a2c3945e07
sukkie <sukkie512@yahoo.co.jp>
2022-10-27 00:54:34 +0900

    fb

--- src/main/java/com/cos/security/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/cos/security/config/auth/PrincipalDetailsService.java
@@ -18,6 +18,7 @@ public class PrincipalDetailsService implements UserDetailsService {
     private UserRepository userRepository;
 
     // Security Session(Authentication(UserDetails))
+    // 함수 종료시 @AuthenticationPrincipal 어노테이션이 만들어 진다.
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserModel userModel = userRepository.findByUsername(username);
--- src/main/java/com/cos/security/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/cos/security/config/oauth/PrincipalOauth2UserService.java
@@ -23,6 +23,7 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
     private UserRepository userRepository;
 
     // FB으로 부터 받은 userRequest 데이터에 대한 후처리되는 함수
+    // 함수 종료시 @AuthenticationPrincipal 어노테이션이 만들어 진다.
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
