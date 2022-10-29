cd0c19ebee2844084cc3bec8b1f0d3b8e686d8b1
sukkie <sukkie512@yahoo.co.jp>
2022-10-29 23:52:16 +0900

    facebook login

--- src/main/java/com/cos/security/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/cos/security/config/oauth/PrincipalOauth2UserService.java
@@ -1,6 +1,9 @@
 package com.cos.security.config.oauth;
 
 import com.cos.security.config.auth.PrincipalDetails;
+import com.cos.security.config.oauth.provider.FacebookUserInfo;
+import com.cos.security.config.oauth.provider.GoogleUserInfo;
+import com.cos.security.config.oauth.provider.OAuth2UserInfo;
 import com.cos.security.model.UserModel;
 import com.cos.security.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -34,11 +37,21 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         OAuth2User oauth2User = super.loadUser(userRequest);
         log.info("getAuthorities : " + oauth2User.getAuthorities());
 
-        String provider = userRequest.getClientRegistration().getRegistrationId();
-        String providerId = oauth2User.getAttribute("sub");
+        // 회원 가입 진행
+        OAuth2UserInfo oAuth2UserInfo = null;
+        if (userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+            oAuth2UserInfo = new GoogleUserInfo(oauth2User.getAttributes());
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
+            oAuth2UserInfo = new FacebookUserInfo(oauth2User.getAttributes());
+        } else {
+            log.error("only support facebook or google");
+        }
+
+        String provider = oAuth2UserInfo.getProvider();
+        String providerId = oAuth2UserInfo.getProviderId();
         String username = provider + "_" + providerId;
         String password = bCryptPasswordEncoder.encode("test");
-        String email = oauth2User.getAttribute("email");
+        String email = oAuth2UserInfo.getEmail();
         String role = "ROLE_USER";
 
         UserModel userEntity = userRepository.findByUsername(username);
